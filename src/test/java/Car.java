57abb6003441a460562c979bce68d045771f77eb
yasothan2001 <yasothanr.21@uom.lk>
2025-02-19 22:50:51 +0530

    Car class initiated

---
+++ src/test/java/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    
+}
